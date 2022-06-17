867f3638c43e0ca9f724dc827722391286e59ac8
PRSMendis <rayhan.mendis@gmail.com>
2022-06-17 10:47:46 +1000

    using react-hook testing library to simplify things

---
+++ src/__tests__/useBreedList.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+ import {expect, test} from '@jest/globals';
+ import { renderHook } from '@testing-library/react-hooks';
+ import useBreedList from '../useBreedList';
+
+
+
+ test("gives an empty list with no animal", async() => {
+    const {result} = render(()=> useBreedList(""))
+    const [breedList, status] = result.current;
+    expect(breedList).toHaveLength(0);
+    expect(status).toBe("unloaded");
+
+ }
+ )
