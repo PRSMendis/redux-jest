6beb86c69bfdf8b1edfb3fd6d681888334b0c42e
PRSMendis <rayhan.mendis@gmail.com>
2022-06-17 10:59:49 +1000

    working API mock test

--- src/__tests__/useBreedList.test.js
+++ src/__tests__/useBreedList.test.js
@@ -4,16 +4,44 @@
 
 
  import {expect, test} from '@jest/globals';
+import { render } from '@testing-library/react';
  import { renderHook } from '@testing-library/react-hooks';
  import useBreedList from '../useBreedList';
 
 
 
  test("gives an empty list with no animal", async() => {
-    const {result} = render(()=> useBreedList(""))
+    const {result} = renderHook(()=> useBreedList(""))
     const [breedList, status] = result.current;
     expect(breedList).toHaveLength(0);
     expect(status).toBe("unloaded");
 
  }
  )
+
+test("gives back breedwith an animal", async () => {
+    const breeds = [
+        "Havanese", 
+        "Bichon Frise",
+        "Poodle",
+        "Maltest",
+        "Golden Retriever",
+        "Labrador",
+        "Husky"
+    ]
+
+    fetch.mockResponseOnce(
+        JSON.stringify({
+            animal:"dog",
+            breeds,
+        })
+    )
+
+    const {result, waitForNextUpdate} = renderHook(()=> useBreedList("dog"))
+
+    await waitForNextUpdate();
+
+    const [breedList, status] = result.current;
+    expect(status).toBe("loaded");
+    expect(breedList).toEqual(breeds);
+})
